5e823bd92cddc012cb00bb6db2e00c17bf7e6f4e
Luis Ramón López <lrlopez@gmail.com>
2015-11-22 23:59:27 +0100

    Entidad enumeración

---
+++ src/AppBundle/Entity/Enumeration.php
@@ -0,0 +1,54 @@
+<?php
+/*
+  ÁTICA - Aplicación web para la gestión documental de centros educativos
+
+  Copyright (C) 2015: Luis Ramón López López
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see [http://www.gnu.org/licenses/].
+*/
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Enumeration
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     * @var string
+     */
+    protected $description;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Module")
+     * @ORM\JoinColumn(referencedColumnName="name")
+     */
+    protected $module;
+}
