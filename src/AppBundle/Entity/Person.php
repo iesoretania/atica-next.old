1686c60dc145a7ebab940fedf9bce918ecba00e0
Luis Ramón López <lrlopez@gmail.com>
2015-11-22 01:35:28 +0100

    Añadidas entidades iniciales de persona y usuario

---
+++ src/AppBundle/Entity/Person.php
@@ -0,0 +1,259 @@
+<?php
+/*
+  ÁTICA - Aplicación web para la gestión documental de centros educativos
+
+  Copyright (C) 2015: Luis Ramón López López
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see [http://www.gnu.org/licenses/].
+*/
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Person
+{
+    const GENDER_UNKNOWN = 0;
+    const GENDER_MALE = 1;
+    const GENDER_FEMALE = 2;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $firstName;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $lastName;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    protected $gender;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $displayName;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @var string
+     */
+    protected $initials;
+
+    /**
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @ORM\OneToOne(targetEntity="User", mappedBy="person")
+     */
+    protected $user;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return Person
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return Person
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set gender
+     *
+     * @param integer $gender
+     *
+     * @return Person
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * Get gender
+     *
+     * @return integer
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * Set displayName
+     *
+     * @param string $displayName
+     *
+     * @return Person
+     */
+    public function setDisplayName($displayName)
+    {
+        $this->displayName = $displayName;
+
+        return $this;
+    }
+
+    /**
+     * Get displayName
+     *
+     * @return string
+     */
+    public function getDisplayName()
+    {
+        return $this->displayName;
+    }
+
+    /**
+     * Set initials
+     *
+     * @param string $initials
+     *
+     * @return Person
+     */
+    public function setInitials($initials)
+    {
+        $this->initials = $initials;
+
+        return $this;
+    }
+
+    /**
+     * Get initials
+     *
+     * @return string
+     */
+    public function getInitials()
+    {
+        return $this->initials;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Person
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set user
+     *
+     * @param User $user
+     *
+     * @return Person
+     */
+    public function setUser(User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,7 +1,306 @@
 <?php
+/*
+  ÁTICA - Aplicación web para la gestión documental de centros educativos
+
+  Copyright (C) 2015: Luis Ramón López López
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see [http://www.gnu.org/licenses/].
+*/
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
 /**
- * Created by PhpStorm.
- * User: lrlopez
- * Date: 21/11/15
- * Time: 23:35
+ * @ORM\Entity
  */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Person", inversedBy="user")
+     * @ORM\JoinColumn(unique=true, nullable=false)
+     */
+    protected $person;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @var string
+     */
+    protected $userName;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var \DateTime
+     */
+    protected $tokenValidity;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    protected $active;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    protected $globalAdministrator;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userName
+     *
+     * @param string $userName
+     *
+     * @return User
+     */
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+
+        return $this;
+    }
+
+    /**
+     * Get userName
+     *
+     * @return string
+     */
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     *
+     * @return User
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set tokenValidity
+     *
+     * @param \DateTime $tokenValidity
+     *
+     * @return User
+     */
+    public function setTokenValidity($tokenValidity)
+    {
+        $this->tokenValidity = $tokenValidity;
+
+        return $this;
+    }
+
+    /**
+     * Get tokenValidity
+     *
+     * @return \DateTime
+     */
+    public function getTokenValidity()
+    {
+        return $this->tokenValidity;
+    }
+
+    /**
+     * Set active
+     *
+     * @param boolean $active
+     *
+     * @return User
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * Get active
+     *
+     * @return boolean
+     */
+    public function isActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * Set globalAdministrator
+     *
+     * @param boolean $globalAdministrator
+     *
+     * @return User
+     */
+    public function setGlobalAdministrator($globalAdministrator)
+    {
+        $this->globalAdministrator = $globalAdministrator;
+
+        return $this;
+    }
+
+    /**
+     * Get globalAdministrator
+     *
+     * @return boolean
+     */
+    public function isGlobalAdministrator()
+    {
+        return $this->globalAdministrator;
+    }
+
+    /**
+     * Get active
+     *
+     * @return boolean
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * Get globalAdministrator
+     *
+     * @return boolean
+     */
+    public function getGlobalAdministrator()
+    {
+        return $this->globalAdministrator;
+    }
+
+    /**
+     * Set person
+     *
+     * @param Person $person
+     *
+     * @return User
+     */
+    public function setPerson(Person $person = null)
+    {
+        $this->person = $person;
+
+        return $this;
+    }
+
+    /**
+     * Get person
+     *
+     * @return Person
+     */
+    public function getPerson()
+    {
+        return $this->person;
+    }
+}
