d40a7eaf916f2f6d26eb78bb190a1366b9cd33b2
Luis Ramón López <lrlopez@gmail.com>
2016-01-07 19:28:13 +0100

    Menú de usuario. Entrada obligatoria en la aplicación

--- src/IesOretania/AticaCoreBundle/Entity/User.php
+++ src/IesOretania/AticaCoreBundle/Entity/User.php
@@ -40,7 +40,7 @@ class User implements UserInterface, \Serializable
     protected $id;
 
     /**
-     * @ORM\OneToOne(targetEntity="Person", inversedBy="user")
+     * @ORM\OneToOne(targetEntity="Person", inversedBy="user",fetch="EAGER")
      * @ORM\JoinColumn(unique=true, nullable=false)
      * @var Person
      */
