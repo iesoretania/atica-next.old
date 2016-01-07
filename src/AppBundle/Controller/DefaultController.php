05d958e37a9ccd7e677bb32e5064a6c1a49e62cf
Luis Ramón López <lrlopez@gmail.com>
2016-01-08 00:52:37 +0100

    Introducido el menú de administración

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -13,5 +14,14 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('default/frontpage.html.twig');
+    }
+
+    /**
+     * @Route("/admin", name="admin_menu")
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function adminIndexAction()
+    {
+        return $this->render('default/admin.html.twig');
     }
 }
