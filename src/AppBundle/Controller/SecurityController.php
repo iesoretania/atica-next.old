bd5e7116e8bcf14a9d8f8b1d8bb302d1af6d805c
Luis Ramón López <lrlopez@gmail.com>
2016-01-20 00:43:05 +0100

    Implementación inicial del sistema de restablecimiento de contraseña

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -2,11 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use IesOretania\AticaCoreBundle\Entity\Membership;
+use IesOretania\AticaCoreBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class SecurityController extends Controller
 {
@@ -46,6 +49,9 @@ class SecurityController extends Controller
             // comprobar que está asociada al usuario
             $em = $this->getDoctrine()->getManager();
 
+            /**
+             * @var Membership|null
+             */
             $membership = $em->getRepository('AticaCoreBundle:Membership')->findOneBy(
                 [
                     'user' => $this->getUser(),
@@ -69,22 +75,72 @@ class SecurityController extends Controller
     }
 
     /**
-     * @Route("/recuperacion", name="login_password_recovery", methods={"GET", "POST"})
+     * @Route("/restablecer", name="login_password_reset", methods={"GET", "POST"})
      */
     public function passwordRecoveryAction(Request $request)
     {
-        /** @var Session $session */
-        $session = $this->get('session');
+        $error = '';
+        $email = $request->get('email');
 
-        // ¿se ha seleccionado una organización?
-        if ($request->isMethod('POST')) {
+        // ¿se ha enviado una dirección?
+        if ($email && $request->isMethod('POST')) {
+            // comprobar que está asociada a un usuario
+            /**
+             * @var User|null
+             */
+            $user = $this->getDoctrine()->getManager()->getRepository('AticaCoreBundle:User')->findOneBy(['email' => $email]);
+            if (null === $user) {
+                $error = $this->get('translator')->trans('form.reset.notfound', [], 'security');
+            } else {
+                // almacenar como último correo electrónico el indicado
+                $this->get('session')->set('_security.last_username', $email);
 
-            // comprobar que está asociada al usuario
-            $em = $this->getDoctrine()->getManager();
+                // obtener tiempo de expiración del token
+                $expire = (int)($this->getParameter('password_reset.expire'));
+
+                // comprobar que no se ha generado un token hace poco
+                if ($user->getToken() && $user->getTokenValidity() > new \DateTime()) {
+                    $error = $this->get('translator')->trans('form.reset.wait', ['%expiry%' => $expire], 'security');
+                }
+                else {
+                    // generar un nuevo token
+                    $user->setToken(sha1(random_bytes(32)));
+
+                    // calcular fecha de expiración del token
+                    $validity = new \DateTime();
+                    $validity->add(new \DateInterval('PT' . $expire . 'M'));
+                    $user->setTokenValidity($validity);
+
+                    // guardar token
+                    $this->get('doctrine')->getManager()->flush();
+
+                    // enviar correo
+                    if (0 === $this->get('app.mailer')->sendEmail([$user],
+                            ['id' => 'form.reset.email.subject', 'parameters' => []],
+                            [
+                                'id' => 'form.reset.email.body',
+                                'parameters' => [
+                                    '%name%' => $user->getPerson()->getFirstName(),
+                                    '%link%' => $this->generateUrl('frontpage', [],
+                                        UrlGeneratorInterface::ABSOLUTE_URL),
+                                    '%expiry%' => $expire
+                                ]
+                            ], 'security')
+                    ) {
+                        $this->addFlash('error', $this->get('translator')->trans('form.reset.error', [], 'security'));
+                    } else {
+                        $this->addFlash('success', $this->get('translator')->trans('form.reset.sent', ['%email%' => $email], 'security'));
+                        return $this->redirectToRoute('login');
+                    }
+                }
+            }
         }
 
         return $this->render(
-            ':security:login_password_recovery.html.twig'
+            ':security:login_password_reset.html.twig', [
+                'last_username' => $this->get('session')->get('_security.last_username', ''),
+                'error' => $error
+            ]
         );
     }
 
