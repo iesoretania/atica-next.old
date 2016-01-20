92dba32a73da740fb8e60b62ce658cb95d1ea8f0
Luis Ramón López <lrlopez@gmail.com>
2016-01-21 00:54:34 +0100

    Completada funcionalidad de restablecimiento de contraseña

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -30,7 +30,7 @@ class SecurityController extends Controller
             'security/login.html.twig',
             array(
                 'last_username' => $lastUsername,
-                'error' => $error,
+                'login_error' => $error,
             )
         );
     }
@@ -77,7 +77,7 @@ class SecurityController extends Controller
     /**
      * @Route("/restablecer", name="login_password_reset", methods={"GET", "POST"})
      */
-    public function passwordRecoveryAction(Request $request)
+    public function passwordResetRequestAction(Request $request)
     {
         $error = '';
         $email = $request->get('email');
@@ -101,10 +101,10 @@ class SecurityController extends Controller
                 // comprobar que no se ha generado un token hace poco
                 if ($user->getToken() && $user->getTokenValidity() > new \DateTime()) {
                     $error = $this->get('translator')->trans('form.reset.wait', ['%expiry%' => $expire], 'security');
-                }
-                else {
+                } else {
                     // generar un nuevo token
-                    $user->setToken(base64_encode(random_bytes(30)));
+                    $token = bin2hex(random_bytes(16));
+                    $user->setToken($token);
 
                     // calcular fecha de expiración del token
                     $validity = new \DateTime();
@@ -121,7 +121,8 @@ class SecurityController extends Controller
                                 'id' => 'form.reset.email.body',
                                 'parameters' => [
                                     '%name%' => $user->getPerson()->getFirstName(),
-                                    '%link%' => $this->generateUrl('frontpage', [],
+                                    '%link%' => $this->generateUrl('login_password_reset_do',
+                                        ['userId' => $user->getId(), 'token' => $token],
                                         UrlGeneratorInterface::ABSOLUTE_URL),
                                     '%expiry%' => $expire
                                 ]
@@ -129,7 +130,8 @@ class SecurityController extends Controller
                     ) {
                         $this->addFlash('error', $this->get('translator')->trans('form.reset.error', [], 'security'));
                     } else {
-                        $this->addFlash('success', $this->get('translator')->trans('form.reset.sent', ['%email%' => $email], 'security'));
+                        $this->addFlash('success',
+                            $this->get('translator')->trans('form.reset.sent', ['%email%' => $email], 'security'));
                         return $this->redirectToRoute('login');
                     }
                 }
@@ -139,6 +141,59 @@ class SecurityController extends Controller
         return $this->render(
             ':security:login_password_reset.html.twig', [
                 'last_username' => $this->get('session')->get('_security.last_username', ''),
+                'error' => $error
+            ]
+        );
+    }
+
+    /**
+     * @Route("/restablecer/{userId}/{token}", name="login_password_reset_do", methods={"GET", "POST"})
+     */
+    public function passwordResetAction(Request $request, $userId, $token)
+    {
+        /**
+         * @var User|null
+         */
+        $user = $this->getDoctrine()->getManager()->getRepository('AticaCoreBundle:User')->findOneBy([
+            'id' => $userId,
+            'token' => $token
+        ]);
+
+        if (null === $user || ($user->getTokenValidity() < new \DateTime())) {
+            $this->addFlash('error', $this->get('translator')->trans('form.reset.notvalid', [], 'security'));
+            return $this->redirectToRoute('login');
+        }
+
+        $data = [
+            'password' => '',
+            'repeat' => ''
+        ];
+
+        $form = $this->createForm('AppBundle\Form\Type\NewPasswordType', $data);
+
+        $form->handleRequest($request);
+
+        $error = '';
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            //codificar la nueva contraseña y asignarla al usuario
+            $password = $this->container->get('security.password_encoder')
+                ->encodePassword($user, $form->get('newPassword')->get('first')->getData());
+            $user->setPassword($password)->setToken(null)->setTokenValidity(null);
+            $this->getDoctrine()->getManager()->flush();
+
+            // indicar que los cambios se han realizado con éxito y volver a la página de inicio
+            $message = $this->get('translator')->trans('passwordChanged', [], 'user');
+            $this->addFlash('success', $message);
+            return new RedirectResponse(
+                $this->generateUrl('frontpage')
+            );
+        }
+
+        return $this->render(
+            ':security:login_password_new.html.twig', [
+                'user' => $user,
+                'form' => $form->createView(),
                 'error' => $error
             ]
         );
